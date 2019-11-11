16f61e8e843d1942eb87e2c2223a664957e36199
Abraham <cedenoabraham@gmail.com>
2019-11-11 00:56:48 -0300

    login style changes

--- client/src/components/login/index.tsx
+++ client/src/components/login/index.tsx
@@ -2,9 +2,9 @@ import * as React from 'react';
 import "./styles.scss";
 
 import useForm from 'react-hook-form';
-import { Form, Input, Button } from 'semantic-ui-react';
+import { Form, Button } from 'semantic-ui-react';
 
-export const Login: React.FC<{}> = () => {
+const Login: React.FC<{}> = () => {
     const { register, handleSubmit, errors } = useForm();
 
     const onSubmit = values => {
@@ -13,23 +13,39 @@ export const Login: React.FC<{}> = () => {
 
     return (
         <div className="login">
-            <h2>Login</h2>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                
-                <Form.Field>
-                    <label>Username</label>
-                    <input placeholder='Username' ref={register} />
-                </Form.Field>
+            <div className="login__form">
+                <h2>Login</h2>
+                <div>
+                    Don't have an account yet ? <a href="/signup">Sign up</a>
+                </div>
+                <Form onSubmit={handleSubmit(onSubmit)}>
+                    
+                    <Form.Field>
+                        <label>Username</label>
+                        <input 
+                            name="username" 
+                            placeholder="Username" 
+                            ref={register({ required: true })} />
+                            {errors.username && 'Username is required.'}
+                    </Form.Field>
 
-                <Form.Field>
-                    <label>Password</label>
-                    <input placeholder='Password' ref={register} />
-                </Form.Field>
+                    <Form.Field>
+                        <label>Password</label>
+                        <input 
+                            name="password" 
+                            type="password" 
+                            placeholder="Password" 
+                            ref={register({ required: true })} />
+                            {errors.password && 'Password is required.'}
+                    </Form.Field>
 
-                <Button type="submit">
-                    Log In
-                </Button>
-            </Form>
+                    <Button type="submit" color="red">
+                        Log In
+                    </Button>
+                </Form>
+            </div>
         </div>
     )
-}
+};
+
+export { Login };
