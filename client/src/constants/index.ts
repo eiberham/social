0db1e07d3abb24fed79447a6fbd899f57ddd7a10
Abraham <cedenoabraham@gmail.com>
2019-12-03 23:54:24 -0300

    events saga

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -5,4 +5,5 @@ export enum Types {
     USER_LOGOUT_SUCCESS   =   'user/logout_success',
     USER_SIGNUP_REQUEST   =   'user/signup_request',
     USER_SIGNUP_SUCCESS   =   'user/signup_success',
+    GET_EVENTS_REQUEST    =   'event/get_request',
 };
---
+++ client/src/sagas/events.ts
@@ -0,0 +1,25 @@
+import { call, fork, put, takeEvery } from 'redux-saga/effects';
+import * as actions from '../actions/auth';
+import { Types } from '../constants';
+import api from '../api';
+
+function* getEvents(action: actions.IAction){
+    try {
+        const {data} = yield call(api.post, '/events', action.payload);
+        console.log("data: ", data);
+        //yield put(actions.userLoginSuccess({}));
+    } catch(e) {
+        //yield put(actions.errorResponse(e.message))
+    }
+}
+
+
+export function* watchGetEventsRequest() {
+    yield takeEvery(Types.GET_EVENTS_REQUEST, getEvents);
+}
+
+const eventSagas = [
+    fork(watchGetEventsRequest)
+];
+
+export default eventSagas;
