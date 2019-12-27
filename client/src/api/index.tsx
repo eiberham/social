fed814376a1e118029756b525ca987083e8309b2
Abraham <cedenoabraham@gmail.com>
2019-12-26 23:48:44 -0300

    webpack dev server proxy

--- client/src/api/index.tsx
+++ client/src/api/index.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://localhost:3000/api',
+    baseURL: '/api',
     timeout: 1000
 });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -41,9 +41,13 @@ module.exports = {
         disableHostCheck: true,
         host: '127.0.0.1',
         port: 80,
-        /* proxy: {
-            '/api': 'https://localhost:3000'
-        } */
+        compress: true,
+        proxy: {
+            '/api': {
+                target: 'https://localhost:3000',
+                secure: false
+            }
+        }
     },
     plugins: [
         new HtmlWebpackPlugin({ 
