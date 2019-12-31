43aaddec3900d19f0cf84489466be174c26b6e88
Abraham <cedenoabraham@gmail.com>
2019-12-31 01:01:57 -0300

    account settings change ui

--- client/src/components/settings/account/index.tsx
+++ client/src/components/settings/account/index.tsx
@@ -2,13 +2,15 @@ import * as React from 'react';
 import { useState } from 'react';
 import useForm from 'react-hook-form';
 
-import { Form, Button, Segment, Confirm } from 'semantic-ui-react';
+import { Form, Button, Segment, Confirm, Checkbox, Divider, Label } from 'semantic-ui-react';
 
 const Component: React.FC<{}> = props => {
     const { register, handleSubmit, errors } = useForm();
     const [open, setOpen] = useState(false);
-    const onSubmit = () => {
-        console.log("form's been submitted");
+    const [changePassword, setChangePassword] = useState(false);
+
+    const onSubmit = ({current, password, repeat}, e) => {
+        console.log("form's been submitted: ", current, password, repeat);
     };
 
     const onDeleteAccount = () => {
@@ -17,21 +19,61 @@ const Component: React.FC<{}> = props => {
 
     return (
         <React.Fragment>
-            <Form onSubmit={handleSubmit(onSubmit)}>
-                <Form.Field inline>
-                    {/* <label>Delete Account</label>
-                    <div className="ui fitted toggle checkbox">
-                        <input type="checkbox" className="hidden" readOnly tabIndex={0} />
-                        <label></label>
-                    </div> */}
-                    
-                    <Button type="submit" color="green" fluid>
-                        Save
-                    </Button>
+            <Checkbox 
+                slider 
+                label={{ children: 'Change Password' }}
+                checked={changePassword}
+                onChange={() => setChangePassword(!changePassword)}    
+            />
 
-                </Form.Field>
-            </Form>
-            <Segment color='red'>
+            <Divider horizontal></Divider>
+
+            {changePassword && ( 
+                <Form onSubmit={handleSubmit(onSubmit)}>
+
+                    <Form.Field required>
+                        <label>Current Password</label>
+                        <input 
+                            name="current" 
+                            type="password" 
+                            placeholder="Password"
+                            ref={register({ required: true })} />
+                            {errors.password && 'Password is required.'}
+                    </Form.Field>
+
+                    <Form.Field required>
+                        <label>Desired Password</label>
+                        <input 
+                            name="password" 
+                            type="password" 
+                            placeholder="Password"
+                            ref={register({ required: true })} />
+                            {errors.newpass && 'Password is required.'}
+                    </Form.Field>
+
+                    <Form.Field required>
+                        <label>Repeat Password</label>
+                        <input 
+                            name="repeat" 
+                            type="password" 
+                            placeholder="Password"
+                            ref={register({ required: true })} />
+                            {errors.newpass && 'Password is required.'}
+                    </Form.Field>
+
+                    <Form.Field inline>
+                        <Button type="submit" color="green" fluid>
+                            Save
+                        </Button>
+
+                    </Form.Field>
+                </Form>
+            )}
+            <Segment color='red' raised>
+                <Label as='a' color='red' ribbon>
+                    Danger Zone
+                </Label>
+                <Divider clearing />
                 <Button type="submit" color="red" fluid onClick={() => setOpen(true)}>
                     Delete Account
                 </Button>
