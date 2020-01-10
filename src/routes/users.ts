b6a7b88797a474d027f47ba1addd0728d35176b5
Abraham <cedenoabraham@gmail.com>
2020-01-10 00:32:09 -0300

    not working yet, it's frustrating

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,16 +1,16 @@
 import { Request, Response, Router } from "express";
 import User from '../models/user';
-import bcrypt from "bcrypt";
 
 const router: Router = Router();
 
 // TODO: finish this endpoint
-router.post(`/:id/change_password`, async (req: Request, res: Response) => {
+router.post(`/:id/change_password`, (req: Request, res: Response) => {
     const { id } = req.params;
-    const { current, password, repeat } = req.body;
-    const salt = 10;
-    // tslint:disable-next-line:no-console
-    console.log(id, current, password, repeat);
+    const {
+        current: username,
+        password,
+        repeat
+    } = req.body;
 
     if ( password !== repeat ) {
         res.status(401).json({
@@ -20,31 +20,22 @@ router.post(`/:id/change_password`, async (req: Request, res: Response) => {
         return;
     }
 
-    bcrypt.hash(current, salt, async (err, hash) => {
-        if ( err ) throw err;
-        if ( hash ) {
-            const user = await User.findOne({
-                where: {
-                    id,
-                    password: hash
+    User.findOne({
+        where: {
+            username
+        }
+    }).then( user => {
+        if ( user ) {
+            user.update({ password }).then(( updated ) => {
+                if ( updated ){
+                    res.status(200).json({
+                        code: 200,
+                        message: 'Resource Updated'
+                    });
                 }
-            });
-
-            if ( user ) {
-                user.update({ password }).then(( updated ) => {
-                    if ( updated ){
-                        res.status(200).json({
-                            code: 200,
-                            message: 'Resource Updated'
-                        });
-                    }
-                })
-            }
+            })
         }
     });
-    /* res.status(200).json({
-        message: 'All good cowboy'
-    }) */
 });
 
 export default router;
